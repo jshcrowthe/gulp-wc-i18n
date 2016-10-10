372248319fd581f48e2920a5051786d0552a4495
Josh Crowther <jshcrowthe@users.noreply.github.com>
2016-10-09 21:30:15 -0600

    Add wc-i18n V2 support (#1)
    * Refactor to add optional wc-i18n v2 support
    
    * Refactor filtering to be more strict around i18n injection
    
    * Remove deprecated Node.js version from test harness
    
    * Refactor to account for v2 api changes

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var dom5 = require('dom5');
 var path = require('path');
 var fs = require('fs');
 var q = require('q');
+var ast = require('ast-query');
 var pred = dom5.predicates;
 
 var srcTagFinder = pred.AND(
@@ -17,6 +18,10 @@ var srcTagFinder = pred.AND(
   ),
   pred.hasAttr('locale-dir')
 );
+
+var scriptTagFinder = pred.AND(
+  pred.hasTagName('script')
+)
 
 var slashRegex = /([^/]+$)/;
 var localeRegex = /.*_(\w{2}).json/;
@@ -39,54 +44,117 @@ var fetchJSONContent = function(file) {
   return returnObj;
 };
 
-var getNewContents = function(localePath, componentName, doc, el) {
+var getTranslations = function(localePath, componentName) {
   var dfd = q.defer();
 
   glob(localePath + '/' + componentName + '_' + '*.json', function(err, files) {
-    var srcLocales = files.map(fetchJSONContent);
-    dom5.removeAttribute(el, 'locale-dir');
-    dom5.setAttribute(el, 'src-locales', JSON.stringify(srcLocales.reduce(flattenArray, {})));
-    dfd.resolve(dom5.serialize(doc));
+    var srcLocales = files.map(fetchJSONContent).reduce(flattenArray, {});
+    dfd.resolve(srcLocales);
   });
 
   return dfd.promise;
 };
 
-module.exports = function () {
-  var newline = gutil.linefeed;
-   var buildClosure = function() {
-     return function(file, encoding, callback) {
-      if (file.isNull()) {
-        this.push(file);
-        return callback();        
-      }
+var wcI18nV1 = function(file, encoding, callback) {
+  if (file.isNull()) {
+    this.push(file);
+    return callback();        
+  }
 
-      if (file.isStream()) {
-        this.emit('error', new PluginError(PLUGIN_NAME, PLUGIN_NAME + ': Streaming not supported'));
-        return callback();
-      }
+  if (file.isStream()) {
+    this.emit('error', new PluginError(PLUGIN_NAME, PLUGIN_NAME + ': Streaming not supported'));
+    return callback();
+  }
 
-      if (file.isBuffer()) {
-        var doc = dom5.parseFragment(String(file.contents));
-        var el = dom5.query(doc, srcTagFinder)
-        if (el) {
-          var componentName = dom5.getAttribute(el, 'domain');
-          var locationAttr = dom5.getAttribute(el, 'locale-dir');
-          var localePath = (locationAttr === '[[localeDir()]]' || locationAttr === '{{localeDir()}}') ? path.resolve(file.path, '../locales') : null;
-          if (localePath) {
-            return getNewContents(localePath, componentName, doc, el)
-              .then(function(contents) {
-                file.contents = new Buffer(contents);
-                this.push(file);
-                return callback();
-              }.bind(this));
-          }
+  if (file.isBuffer()) {
+    var doc = dom5.parseFragment(String(file.contents));
+    var el = dom5.query(doc, srcTagFinder)
+    if (el) {
+      var componentName = dom5.getAttribute(el, 'domain');
+      var locationAttr = dom5.getAttribute(el, 'locale-dir');
+      var localePath = (locationAttr === '[[localeDir()]]' || locationAttr === '{{localeDir()}}') ? path.resolve(file.path, '../locales') : null;
+      if (localePath) {
+        return getTranslations(localePath, componentName)
+          .then(function(translations) {
+            dom5.removeAttribute(el, 'locale-dir');
+            dom5.setAttribute(el, 'src-locales', JSON.stringify(translations));
+            return dom5.serialize(doc);
+          })
+          .then(function(contents) {
+            file.contents = new Buffer(contents);
+            this.push(file);
+            return callback();
+          }.bind(this));
+      }
+    }
+  }
+  this.push(file);
+  return callback();
+};
+
+var wcI18nV2 = function(file, encoding, callback) {
+  if (file.isNull()) {
+    this.push(file);
+    return callback();        
+  }
+
+  if (file.isStream()) {
+    this.emit('error', new PluginError(PLUGIN_NAME, PLUGIN_NAME + ': Streaming not supported'));
+    return callback();
+  }
+
+  if (file.isBuffer()) {
+    var doc = dom5.parseFragment(String(file.contents));
+    var scripts = dom5.queryAll(doc, scriptTagFinder);
+    // do stuff
+    var promises = scripts.map(function(script) {
+      return {
+        script: script,
+        text: dom5.getTextContent(script)
+      }
+    }).filter(function(jsStringObj) {
+      return /\bWCI18n\b/.test(jsStringObj.text);
+    }).map(function(jsStringObj) {
+      return {
+        script: jsStringObj.script,
+        tree: ast(jsStringObj.text)
+      };
+    }).filter(function(treeObj) {
+      return treeObj.tree.callExpression('WCI18n').length > 0;
+    }).map(function(treeObj) {
+      var componentName = treeObj.tree.callExpression('Polymer').arguments.at(0).key('is').value();
+      return getTranslations(path.resolve(file.path, '../locales'), componentName).then(function(translations) {
+        return {
+          script: treeObj.script,
+          tree: treeObj.tree,
+          translations: translations
         }
-      }
-      this.push(file);
-      return callback();
-    };
-  };
+      });
+    }).filter(function(promises) {
+      return !!promises;
+    });
 
-  return through.obj(buildClosure());
+    Promise.all(promises)
+      .then(function(i18nObjArray) {
+        i18nObjArray.forEach(function(i18nObj) {
+          var args = i18nObj.tree.callExpression('WCI18n').arguments;
+          args.push(JSON.stringify(i18nObj.translations));
+          dom5.setTextContent(i18nObj.script, i18nObj.tree.toString());
+        });
+      })
+      .then(function() {
+        return dom5.serialize(doc);
+      })
+      .then(function(contents) {
+        file.contents = new Buffer(contents);
+        this.push(file);
+        return callback();
+      }.bind(this));
+  }
+};
+
+module.exports = function (config) {
+  var version = config && config.version || 1;
+  var handler = version === 2 ? wcI18nV2 : wcI18nV1;
+  return through.obj(handler);
 };
